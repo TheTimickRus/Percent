818004429bb567066f273c4cc66210822e7731db
Андрей Тимофеев <TheTimickRus@outlook.com>
2021-02-02 18:33:28 +0300

    Init #1

--- src/Percent/Properties/AssemblyInfo.cs
+++ src/Percent/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Timick")]
 [assembly: AssemblyProduct("Калькулятор %")]
-[assembly: AssemblyCopyright("Copyright © Timick 2019")]
+[assembly: AssemblyCopyright("Copyright © Timick 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
